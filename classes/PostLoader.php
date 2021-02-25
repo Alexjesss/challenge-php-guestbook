028bd6fde595a55ade4c22151a22015061d632a0
Alexandra Banica <banica9221@hotmail.be>
2021-02-25 16:04:57 +0100

    changed method for storing messages

--- classes/PostLoader.php
+++ classes/PostLoader.php
@@ -3,5 +3,12 @@
 
 class PostLoader
 {
-//toon versch posts
+    public function storeAndLoadMessages(Post $post)
+    {
+        $file = 'saveContent.txt';
+        try {
+            $json = json_encode($post->toArray(), JSON_THROW_ON_ERROR,);
+        } catch (JsonException $e) {
+        }
+    }
 }
