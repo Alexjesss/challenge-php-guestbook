c2898750e0c6d959794485c37a3b694fad3c9460
Alexandra Banica <banica9221@hotmail.be>
2021-02-25 22:36:03 +0100

    modifying store and load input method

--- classes/PostLoader.php
+++ classes/PostLoader.php
@@ -3,12 +3,28 @@
 
 class PostLoader
 {
-    public function storeAndLoadMessages(Post $post)
+    private array $allPosts = [];
+
+    public function storeAndLoadMessages(Post $post): void
     {
         $file = 'saveContent.txt';
+        file_put_contents($file, $post);
         try {
-            $json = json_encode($post->toArray(), JSON_THROW_ON_ERROR,);
+            $json = json_encode($file, JSON_THROW_ON_ERROR,);
         } catch (JsonException $e) {
         }
+        file_get_contents($file,$json);
     }
+
+    public function getAllPosts(): array
+    {
+        return $this->allPosts;
+    }
+
+    public function setAllPosts(array $allPosts): void
+    {
+        $this->allPosts = $allPosts;
+    }
+
+
 }
