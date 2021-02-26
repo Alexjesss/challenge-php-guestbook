8ac6740391acc3e1799c550b0afe031be52f0f3c
Alexandra Banica <banica9221@hotmail.be>
2021-02-26 15:01:40 +0100

    added new function to display messages

--- classes/PostLoader.php
+++ classes/PostLoader.php
@@ -4,6 +4,7 @@
 class PostLoader
 {
     private array $allPosts = [];
+    public const MAXIMUM_POSTS = 20;
 
     public function storeMessages(Post $post): void
     {
@@ -11,20 +12,24 @@ class PostLoader
         try {
             $arrayPost = $post->toArray();
             $temp = json_decode(file_get_contents($file), true, 512, JSON_THROW_ON_ERROR);
-            array_unshift($temp,$arrayPost);
+            array_unshift($temp, $arrayPost);
             file_put_contents($file, json_encode($temp, JSON_THROW_ON_ERROR));
+            $this->allPosts = $temp;
         } catch (JsonException $e) {
         }
     }
 
-    public function displayMessages(){
-        $counter =0;
-        foreach($temp as $arr){
-            if($counter<10){
-                echo $arr.PHP_EOL;
-            }
-            $counter++;
+    public function displayMessages(): void
+    {
+        for ($i = 0; $i < min(self::MAXIMUM_POSTS, count($this->allPosts)); $i++) {
+            $this->postedMessages($this->allPosts[$i]);
         }
+    }
+
+    public function postedMessages(array $array)
+    {
+
+        echo $array['title'] . ' ' . $array['date'] . " " . $array['content'] . " " . $array['authorName'];
     }
 
 
