aefd4c6b6e93d1d44413ac082b143a45750292b9
Alexandra Banica <banica9221@hotmail.be>
2021-02-26 12:27:22 +0100

    made posts show in file from new to old with function

--- classes/PostLoader.php
+++ classes/PostLoader.php
@@ -5,16 +5,28 @@ class PostLoader
 {
     private array $allPosts = [];
 
-    public function storeAndLoadMessages(Post $post): void
+    public function storeMessages(Post $post): void
     {
         $file = 'saveContent.txt';
-        file_put_contents($file, $post);
         try {
-            $json = json_encode($file, JSON_THROW_ON_ERROR,);
+            $arrayPost = $post->toArray();
+            $temp = json_decode(file_get_contents($file), true, 512, JSON_THROW_ON_ERROR);
+            array_unshift($temp,$arrayPost);
+            file_put_contents($file, json_encode($temp, JSON_THROW_ON_ERROR));
         } catch (JsonException $e) {
         }
-        file_get_contents($file,$json);
     }
+
+    public function displayMessages(){
+        $counter =0;
+        foreach($temp as $arr){
+            if($counter<10){
+                echo $arr.PHP_EOL;
+            }
+            $counter++;
+        }
+    }
+
 
     public function getAllPosts(): array
     {
