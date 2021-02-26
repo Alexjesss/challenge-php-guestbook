172a5040a0a44f84cc7249e4640b13cbeceb8101
Alexandra Banica <banica9221@hotmail.be>
2021-02-26 15:02:41 +0100

    php echo errors,function to display messages

--- index.php
+++ index.php
@@ -13,36 +13,34 @@ $userInput = [
     'content' => "",
 ];
 
-if (($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST['submit'])) {
+if (($_SERVER["REQUEST_METHOD"] === "POST")) {
     if (empty($_POST["title"])) {
-        $errors['title'] = "* input required";
+        $errors['title_error'] = "* input required";
     } elseif (!empty($_POST['title']) && is_numeric($_POST['title'])) {
-        $errors['title'] = "* text characters only";
-}
-else {
+        $errors['title_error'] = "* text characters only";
+    } else {
         $userInput['title'] = modified_input($_POST['title']);
     }
 
-    if (empty($_POST["content"])){
-        $errors['content'] = "* input required";
-    }
-    else {
+    if (empty($_POST["content"])) {
+        $errors['content_error'] = "* input required";
+    } else {
         $userInput['content'] = modified_input($_POST['content']);
     }
 
-    if (empty($_POST["name"])){
-        $errors['content'] = "* input required";
-    }
-    elseif (!empty($_POST['name']) && is_numeric($_POST['name'])) {
-        $errors['name'] = "* text characters only";
+    if (empty($_POST["name"])) {
+        $errors['authorName_error'] = "* input required";
+    } elseif (!empty($_POST['name']) && is_numeric($_POST['name'])) {
+        $errors['authorName_error'] = "* text characters only";
     } else {
         $userInput['name'] = modified_input($_POST['name']);
     }
 }
 
+$postLoader = new PostLoader();
+
 if (isset($_POST['submit'])) {
-    $post = new Post($_POST['title'],date('Y-m-d H:i:s'),$_POST['content'],$_POST['authorName']);
-    $postLoader = new PostLoader();
+    $post = new Post($_POST['title'], date('Y-m-d H:i:s'), $_POST['content'], $_POST['authorName']);
     $postLoader->storeMessages($post);
 }
 
@@ -73,19 +71,20 @@ function modified_input($input)
         <div class="form-group col-md-6">
             <label for="title">Title:</label>
             <input type="text" name="title" class="form-control">
-            <span><?php ?></span>
+            <span><?php echo $errors['title_error']; ?></span>
         </div>
         <div class="form-group col-md-6">
             <label for="content">Content:</label>
             <input type="text" name="content" class="form-control">
-            <span><?php ?></span>
+            <span><?php echo $errors['content_error']; ?></span>
         </div>
         <div class="form-group col-md-6">
             <label for="authorName">Your name:</label>
             <input type="text" name="authorName" class="form-control">
-            <span><?php ?></span>
+            <span><?php echo $errors['authorName_error']; ?></span>
         </div>
         <button type="submit" name="submit" class="btn btn-primary">Send!</button>
+        <div><?php echo $postLoader->displayMessages(); ?></div>
 </form>
 </body>
 </html>
